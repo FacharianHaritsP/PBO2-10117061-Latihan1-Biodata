77d00b1e23d6e85bef4ab5f11460beebcc4db428
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-09-22 18:18:53 +0700

    Add files via upload

---
+++ src/pb02/pkg10117061/latihan1/biodata/PB0210117061Latihan1Biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pb02.pkg10117061.latihan1.biodata;
+
+/**
+ *
+ * @author Fachrian Harits Pratama
+ * Nama                 : Fachrian Harits Pratama
+ * Kelas                : PBO2
+ * NIM                  : 10117061
+ * Deskripsi program    : Program ini untuk menampilakan value ke layar
+ */
+public class PB0210117061Latihan1Biodata {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR"+"\n"+"-------------------------");
+        System.out.println("NAMA\t\t: Fachrian Harits Pratama");
+        System.out.println("ALAMAT\t\t: jalan venus barat III no 11");
+        System.out.println("TEMPAT LAHIR\t: BANDAR LAMPUNG");
+        System.out.println("TANGGAL LAHIR\t: 11 Agustus 1999");
+        System.out.println("----------------------");
+        System.out.println("HOBI\t\t: main game");
+        System.out.println("MAKES\t\t: mie");
+        System.out.println("MIKES\t\t: susu");
+    }
+    
+}
